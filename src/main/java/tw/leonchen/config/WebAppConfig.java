812d1e34fd7092498815f7992de4964403bee485
KennyShow <thoroughgoingkill@gmail.com>
2022-06-14 16:22:22 +0800

    轉換成java 組態檔案

---
+++ src/main/java/tw/leonchen/config/WebAppConfig.java
@@ -0,0 +1,24 @@
+package tw.leonchen.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@ComponentScan(basePackages = "tw.leonchen")
+@EnableWebMvc
+
+
+//    /* 上面三個等於下面三個設定
+//<context:annotation-config/>
+//<context:component-scan base-package="tw.leonchen"/>
+//<mvc:annotation-driven/>
+public class WebAppConfig implements WebMvcConfigurer {
+
+	@Override  
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+}
