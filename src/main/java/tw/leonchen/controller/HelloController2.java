111ce81c5069f22fb6011038c706a9e83670f4c2
KennyShow <thoroughgoingkill@gmail.com>
2022-06-14 15:13:32 +0800

    Spring MVC

---
+++ src/main/java/tw/leonchen/controller/HelloController2.java
@@ -0,0 +1,41 @@
+package tw.leonchen.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
+@SessionAttributes(names = {"userName"} )
+public class HelloController2 {
+	
+	
+	@RequestMapping(path = "/hello2.controller",method = RequestMethod.GET)  //等於前端的action and 方法
+	public String processAction(@RequestParam("userName") String userName,Model m,SessionStatus status) {
+		//String userName = request.getParameter("userName"); 由上方取代
+		Map<String, String> errors =new HashMap<String,String>();
+		m.addAttribute("errors", errors);  //=request.setAttribute("errors", errors);
+	
+		if (userName==null || userName.length()==0) {
+			errors.put("name", "name is required");
+		}
+	
+		if (errors !=null && !errors.isEmpty()) {
+//		return new  ModelAndView("/form.jsp");
+			return "/form.jsp";
+		}
+		m.addAttribute("userName", userName);
+		
+		status.setComplete();// 這行可以清掉 session的資料
+		return "/success.jsp";
+	}
+
+
+	
+}
